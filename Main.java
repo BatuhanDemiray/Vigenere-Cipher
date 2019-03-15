9fab156756558f8c01b0cc756f9433a777412e50
Batuhan Demiray <batuhan28_@hotmail.com>
2019-03-16 01:30:31 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,40 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* Vigenere Cipher */
+                /* Vigenere Cipher */
         Scanner scan = new Scanner(System.in);
         Cipher cph = new Cipher();
 
-        // ----- MENU ----- //
-        /*while(true) {
-        System.out.println("1)Encrypt 2)Decrypt");
-        System.out.print("Menü Seçiniz: ");
-        int process = scan.nextInt();
-        switch (process) {
-            case 1:
-                System.out.print("Şifrelenecek Kelimeyi Giriniz: ");
-                String encrypt = scan.next();
-                System.out.print("Keyi Giriniz: ");
-                String key = scan.next();
-                String encryptedMessage = cph.encrypt(cph.toUpperString(encrypt), cph.toUpperString(key));
-                System.out.println(encryptedMessage);
-                break;
-            case 2:
-                System.out.print("Şifreli Kelimeyi Giriniz: ");
-                encrypt = scan.next();
-                System.out.print("Keyi Giriniz: ");
-                key = scan.next();
-                cph.decrypt(cph.toUpperString(encrypt), cph.toUpperString(key));
-                break;
-            default:
-                System.out.println("Error!");
-                break;
-        }
-    }*/
         System.out.print("Şifrelenecek Kelimeyi Giriniz: ");
         String encrypt = scan.next();
         System.out.print("Keyi Giriniz: ");
         String key = scan.next();
+        
         String encryptedMessage = cph.encrypt(cph.toUpperString(encrypt), cph.toUpperString(key));
         cph.decrypt(cph.toUpperString(encryptedMessage), cph.toUpperString(key));
 
